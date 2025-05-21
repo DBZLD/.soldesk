e89a397b4fe08b1e8aef6e896810417f2d1774f6
DBZLD <111506781+DBZLD@users.noreply.github.com>
2025-05-21 17:48:59 +0900

    5/21
    jsp
    서블릿 컨트롤러
    v8까지 완료

---
+++ jsp/JspDaoDtoBoardV8/src/main/java/db/ControllerBoard.java
@@ -0,0 +1,29 @@
+package db;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/db/*")
+public class ControllerBoard extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getPathInfo();
+		String nextPage = "";
+		if(action == null) {
+			switch(action) {
+			case"/del":
+				String no=request.getParameter("no");
+				Dao dao = new Dao();
+				dao.del(no);
+				nextPage = "/db/list";
+				break;
+			}
+		}
+	}
+
+}
