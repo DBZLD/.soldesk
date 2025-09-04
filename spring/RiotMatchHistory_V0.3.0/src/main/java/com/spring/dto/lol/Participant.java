6f234da1a8c8b951b5122418e5090c0470157f88
DBZLD <dbzld102@gmail.com>
2025-09-04 17:49:04 +0900

    9/4
    lol api 구조 분석, 제작

--- spring/RiotMatchHistory_V0.3.0/src/main/java/com/spring/dto/lol/Participant.java
+++ spring/RiotMatchHistory_V0.3.0/src/main/java/com/spring/dto/lol/Participant.java
@@ -8,7 +8,7 @@ public class Participant { // 매치의 플레이어 상세 정보가 들어있는 클래스
 	public String riotIdTagline;              // 라이엇 태그라인
 	public int summonerLevel;                 // 소환사 레벨
     public int profileIcon;                   // 프로필 아이콘 ID
-    public int participantId;                 // 플레이어 ID
+    public int participantId;                 // participant배열 내에서의 순서
     
     //플레이어 매치 통계
     public int championId;                    // 챔피언 ID
@@ -16,29 +16,31 @@ public class Participant { // 매치의 플레이어 상세 정보가 들어있는 클래스
     public int champLevel;                    // 챔피언 레벨
     public int champExperience;               // 챔피언 경험치
     public int championTransform;             // 챔피언 변신 상태 (케인 용)
-    public String individualPosition;         // 포지션(top, mid, etc)
-    public String lane;                       // 라인 (TOP, MID, etc)
-    public String role;                       // 역할 (TOP, JUNGLE, etc)
+    public String individualPosition;         // 포지션 (TOP, MID, BOTTOM, UTILITY)
+    public String teamPosition;               // 팀 포지션 (TOP, MID, BOTTOM, UTILITY)
+    public String lane;                       // 라인 (TOP, MIDDLE, JUNGLE, BOTTOM)
+    public String role;                       // 역할 (SOLO, CARRY, SUP, NONE)
     public int kills;                         // 킬 수
     public int deaths;                        // 사망 횟수
     public int assists;                       // 어시스트 횟수
-    public int killingSprees;                 // 연속 킬 수
-    public int totalAllyJungleMinionsKilled;  // 아군 정글 몬스터 처치 수
-    public int totalEnemyJungleMinionsKilled; // 적 정글 몬스터 처치 수
-    public int totalMinionsKilled;            // 총 미니언 처치 수
-    public int largestCriticalStrike;         // 최대 치명타
-    public int largestKillingSpree;           // 최대 연속 킬
-    public int largestMultiKill;              // 최대 멀티킬
-    public int longestTimeSpentLiving;        // 최장 생존 시간
-    public String teamPosition;               // 팀 포지션 (TOP, MID, etc)
-    public int timeCCingOthers;               // 군중 제어(Crowd Control) 시간
-    public int timePlayed;                    // 플레이 시간 (초)
-    public int totalTimeCCDealt;              // 총 군중 제어 시간
-    public int totalTimeSpentDead;            // 사망으로 보낸 시간
     public int tripleKills;                   // 트리플킬 수
     public int quadraKills;                   // 쿼드라킬 수
     public int pentaKills;                    // 펜타킬 수
+    public int unrealKills;                   // 5연속 킬 이상 수
     public int doubleKills;                   // 더블킬 수
+    public int killingSprees;                 // 연속 킬 수
+    public int largestKillingSpree;           // 최대 연속 킬
+    public int largestMultiKill;              // 최대 멀티킬(더블, 트리플)
+    public int totalMinionsKilled;            // 상대 미니언 처치 수
+    public int neutralMinionsKilled;          // 중립 미니언(상대 미니언 제외 전부) 처치 수
+    public int totalAllyJungleMinionsKilled;  // 아군 정글 몬스터 처치 수
+    public int totalEnemyJungleMinionsKilled; // 적 정글 몬스터 처치 수
+    public int largestCriticalStrike;         // 최대 치명타
+    public int timePlayed;                    // 플레이 시간 (초)
+    public int longestTimeSpentLiving;        // 최장 생존 시간
+    public int timeCCingOthers;               // 군중 제어 가한 시간
+    public int totalTimeCCDealt;              // 군중 제어 당한 시간
+    public int totalTimeSpentDead;            // 사망으로 보낸 시간
     public Perks perks;                       // 룬 정보
     public int spell1Casts;                   // q 사용 횟수
     public int spell2Casts;                   // w 사용 횟수
@@ -48,7 +50,7 @@ public class Participant { // 매치의 플레이어 상세 정보가 들어있는 클래스
     public int summoner1Id;                   // 소환사 주문1 ID
     public int summoner2Casts;                // 소환사 주문2 사용 횟수
     public int summoner2Id;                   // 소환사 주문2 ID
-    public Challenges challenges;             // 챌린지 정보
+    public Challenges challenges;             // 플레이어 업적(상세 통계)
     //골드, 아이템
     public int goldEarned;                    // 획득 골드
     public int goldSpent;                     // 사용 골드
@@ -86,6 +88,7 @@ public class Participant { // 매치의 플레이어 상세 정보가 들어있는 클래스
     public int totalHeal;                     // 총 회복량
     public int totalDamageShieldedOnTeammates;// 아군 보호막 총 흡수량
     public int totalHealsOnTeammates;         // 아군 회복량
+    public int totalUnitsHealed;              // 회복한 유닛 수?
     public int physicalDamageDealt;           // 가한 물리 피해량
     public int physicalDamageDealtToChampions;// 챔피언에게 입힌 물리 피해량
     public int physicalDamageTaken;           // 받은 물리 피해량
@@ -96,8 +99,8 @@ public class Participant { // 매치의 플레이어 상세 정보가 들어있는 클래스
     public int trueDamageDealtToChampions;    // 챔피언에게 입힌 고정 피해량
     public int trueDamageTaken;               // 받은 고정 피해량
     public int damageDealtToBuildings;        // 구조물에 입힌 피해량
-    public int damageDealtToObjectives;       // 오브젝트에 입힌 피해량
     public int damageDealtToTurrets;          // 포탑에 입힌 피해량
+    public int damageDealtToObjectives;       // 오브젝트에 입힌 피해량
 
     //오브젝트, 구조물
     public boolean firstBloodAssist;          // 첫 킬 어시스트 여부
@@ -107,28 +110,27 @@ public class Participant { // 매치의 플레이어 상세 정보가 들어있는 클래스
     public int objectivesStolen;              // 오브젝트 스틸 수
     public int objectivesStolenAssists;       // 오브젝트 스틸 어시스트 수
     public int turretKills;                   // 포탑 처치 수
-    public int turretTakedowns;               // 포탑 제거 횟수
+    public int turretTakedowns;               // 포탑 파괴관여
     public int turretsLost;                   // 잃은 포탑 수
     public int baronKills;                    // 바론 처치 횟수
     public int dragonKills;                   // 드래곤 처치 수
-    public int neutralMinionsKilled;          // 중립 몬스터 처치 수
     public int inhibitorKills;                // 억제기 파괴 수
-    public int inhibitorTakedowns;            // 억제기 막타 횟수
+    public int inhibitorTakedowns;            // 억제기 파괴관여
     public int inhibitorsLost;                // 잃은 억제기 수
     public int nexusKills;                    // 넥서스 파괴 수
     public int nexusLost;                     // 잃은 넥서스 수
     public int nexusTakedowns;                // 넥서스 파괴관여
     
     //시야
-    public int detectorWardsPlaced;           // 제어 와드 설치 수
-    public int sightWardsBoughtInGame;        // 시야용 와드 구매 수
     public int visionScore;                   // 시야 점수
-    public int visionWardsBoughtInGame;       // 시야용 와드 구매 수
-    public int wardsKilled;                   // 와드 제거 수
+    public int detectorWardsPlaced;           // 제어 와드 설치 수
+    public int visionWardsBoughtInGame;       // 제어 와드 구매 수
     public int wardsPlaced;                   // 와드 설치 수
+    public int wardsKilled;                   // 와드 제거 수
+    public int sightWardsBoughtInGame;        // 시야용 와드 구매 수(과거 와드 말하는 것 같음)
     
     //팀
-    public int teamId;                        // 팀 ID
+    public int teamId;                        // 팀 ID(블루:100, 레드:200)
     public boolean gameEndedInEarlySurrender; // 조기 항복으로 종료 여부
     public boolean gameEndedInSurrender;      // 항복으로 종료 여부
     public boolean teamEarlySurrendered;      // 팀 조기 항복 여부
@@ -159,7 +161,5 @@ public class Participant { // 매치의 플레이어 상세 정보가 들어있는 클래스
     public int playerSubteamId;               // 서브 팀 ID?
     public Missions missions;                 // 미션 정보?
     public String summonerName;               // 소환사 이름?
-    public int totalUnitsHealed;              // 회복한 유닛 수?
-    public int unrealKills;                   // Unreal 킬 수?
 }
 
--- spring/RiotMatchHistory_V0.3.0/src/main/java/com/spring/dto/lol/Selection.java
+++ spring/RiotMatchHistory_V0.3.0/src/main/java/com/spring/dto/lol/Selection.java
@@ -2,7 +2,7 @@ package com.spring.dto.lol;
 
 public class Selection {
     public int perk;	// 선택한 룬 아이디
-    public int var1;	// ?
-    public int var2;	// ?
-    public int var3;	// ?
+    public int var1;	// 게임 내의 룬 스탯(회복한 체력, 획득한 골드 등등)
+    public int var2;	// 게임 내의 룬 스탯(회복한 체력, 획득한 골드 등등)
+    public int var3;	// 게임 내의 룬 스탯(회복한 체력, 획득한 골드 등등)
 }
